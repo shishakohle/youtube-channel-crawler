61c96c5733bcd4b3bbbfe527b2d990d4d1938b01
Ingo Weigel <me19m022@technikum-wien.at>
2022-06-11 04:00:02 +0200

    Initial commit

---
+++ YoutubeChannelCrawler.py
@@ -0,0 +1,47 @@
+import googleapiclient.discovery
+from googleapiclient.errors import HttpError
+
+
+class YouTubeChannelCrawler:
+    yt_resource = None
+    yt_playlistId = None
+
+    def __init__(self, filepath_yt_api_key, yt_channel_id):
+        # load secret key for the YT API from file
+        with open(filepath_yt_api_key, "r") as file_yt_api_key:
+            yt_api_key = file_yt_api_key.readline().rstrip('\n')
+            file_yt_api_key.close()
+        if not yt_api_key:
+            # this file had no API key
+            return
+        self.yt_resource = googleapiclient.discovery.build("youtube", "v3", developerKey=yt_api_key)
+        request = self.yt_resource.channels().list(part="contentDetails", id=yt_channel_id)
+        response = {}
+        try:
+            response = request.execute()
+        except HttpError:
+            # invalid API key
+            self.yt_resource = None
+        if 'items' in response:
+            self.yt_playlistId = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        else:
+            # invalid channel ID
+            self.yt_playlistId = None
+
+    def latest_video(self):
+        if self.yt_resource is None:
+            # YT resource not ready.
+            return None
+        request = self.yt_resource.playlistItems().list(part="snippet", playlistId=self.yt_playlistId, maxResults=1)
+        try:
+            response = request.execute()
+        except HttpError:
+            # no videos on this channel
+            return None
+        if 'items' in response:
+            video_id = response['items'][0]['snippet']['resourceId']['videoId']
+            video_title = response['items'][0]['snippet']['title']
+            return {'videoId': video_id, 'videoTitle': video_title}
+        else:
+            # no videos on this channel
+            return None
---
+++ usage-example.py
@@ -0,0 +1,26 @@
+from YoutubeChannelCrawler import YouTubeChannelCrawler
+
+
+# YT channel ID of FHTW Podcast
+channel_id = 'UCP4A1An8dNSxcA8Lj55qVvQ'
+
+# YT channel ID of Power TJ
+# channel_id = 'UCDzjkX1p0DNmy6Mt4s29dUw'
+
+# YT channel ID of Lukas Wagner (no videos posted)
+# channel_id = 'UCxAxneUPVLWElaiKunEgpBQ'
+
+# some invalid YT channel ID
+# channel_id = '1234'
+
+yt_channel = \
+    YouTubeChannelCrawler(filepath_yt_api_key='./yt-api-key.txt', yt_channel_id=channel_id)
+
+latest_video = yt_channel.latest_video()
+
+if latest_video is None:
+    print("No video found on the crawled YouTube Channel.")
+else:
+    print("The latest video on crawled YouTube Channel:")
+    print("Title: " + latest_video['videoTitle'])
+    print("URL: https://youtube.com/watch?v=" + latest_video['videoId'])
